3eb81a1b6b66c3a572aa9cd76852d2428450f47c
Edson <edson.lipa@gmail.com>
2015-05-21 16:53:42 -0500

    botones(button)
    estructura inicial y implementacion de la clase botones operativo

--- main.cpp
+++ main.cpp
@@ -1,82 +1,49 @@
-/**********************************
-Window*****************************
-**********************************/
-#include <SFML/Graphics.hpp>
-//#include <SFML/Audio.hpp>
+/*#ifdef SFML_STATIC
+#pragma comment(lib, "glew.lib")
+#pragma comment(lib, "freetype.lib")
+#pragma comment(lib, "jpeg.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "gdi32.lib")
+#endif // SFML_STATIC*/
+
 #include <iostream>
-#include <string>
 
-using namespace std;
-
-class Player
-{
-public:
-    sf::RectangleShape rect; //private
-    float bottom,left,right,top;
-
-    Player(sf::Vector2f position,sf::Vector2f size, sf::Color color)
-    {
-        rect.setPosition(position);
-        rect.setSize(size);
-        rect.setFillColor(color);
-    }
-
-    void update()
-    {
-        bottom = rect.getPosition().y + rect.getSize().y;
-        left = rect.getPosition().x;
-        right = rect.getPosition().x+rect.getSize().x;
-        top = rect.getPosition().y;
-    }
-
-    bool Collision(Player p)
-    {
-        if(right< p.left||left>p.right ||top>p.bottom || bottom<p.top)
-        {
-            return false;
-        }
-        return true;
-    }
-};
+#include <SFML/Graphics.hpp>
+#include "Button.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(640,480,32),"SCRATCH IN DEVELOPMENT");
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Scrutch");
 
-    Player p1(Player(sf::Vector2f(10,10),sf::Vector2f(20,20),sf::Color::Red)),
-    p2(Player(sf::Vector2f(100,100),sf::Vector2f(20,20),sf::Color::Blue));
+	std::string x = "avanza x pasos";
+	sf::Vector2f positionI(50, 50),positionj(150,150),positionk(300,300);
 
-    while(window.isOpen())
-    {
-        sf::Event Event;
-        while(window.pollEvent(Event))
-        {
-            switch(Event.type)
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            }
-        }
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-            p1.rect.move(1.0f,0);
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-            p1.rect.move(-1.0f,0);
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-            p1.rect.move(0,-1.0f);
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-            p1.rect.move(0,1.0f);
+	sf::Texture buttonTexture,buttonClickedTexture;
+	if (!buttonTexture.loadFromFile("button.png") || !buttonClickedTexture.loadFromFile("specials-button.png"))
+	{
+		std::cout << "couldn't open the file" << std::endl;
+	}
 
-        p1.update();
-        p2.update();
 
-        if(p1.Collision(p2)){
-            std::cout<<"Collision"<<endl;
-            p1.rect.move(-0.1f,0);
-        }
-        window.clear(sf::Color(0,240,255));
-        window.draw(p1.rect);
-        window.draw(p2.rect);
-        window.display();
-    }
+	Button adelante(&buttonTexture,&buttonClickedTexture,x,positionI);
+    Button arriba(&buttonTexture,&buttonClickedTexture,x,positionj);
+    Button abajo(&buttonTexture,&buttonClickedTexture,x,positionk);
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(*adelante.getSprite());
+		window.draw(*abajo.getSprite());
+		window.draw(*arriba.getSprite());
+		window.display();
+	}
+
+	return 0;
 }
