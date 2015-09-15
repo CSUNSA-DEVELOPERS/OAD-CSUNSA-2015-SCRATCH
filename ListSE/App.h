a76641bb1ccec149500557fa3f8b9df092e8bf7a
Rubens Edwin <edwinrubens1335@gmail.com>
2015-09-14 21:06:06 -0500

    NUEVAS f
    Funcion imprimir por año, find, requisitos y getRecord retorna un
    string.

--- ListSE/App.h
+++ ListSE/App.h
@@ -1,6 +1,8 @@
 #ifndef APP_H
 #define APP_H
 #include "Table.h"
+#include <string>
+#include <vector>
 
 template <class T>
 class Aplicacion
@@ -10,10 +12,12 @@ class Aplicacion
         virtual ~Aplicacion();
         void start();
         void startPrinting();
-        void addPre_rqs();
+        void addPre_rqs(char *key);
+        vector<string>* find_prerqs(char *key);
         void print_for_code();
         void addSubject();
         void find_Subject();
+        char* year_key(char *key);
     protected:
     private:
         Table<Record> *Cursos;
@@ -41,6 +45,26 @@ void Aplicacion<T>::start()
     Cursos->index();
     Requisitos->index();
     startPrinting();
+    char temp[9]= "CS3CB111";
+    addPre_rqs(temp);
+}
+
+template <class T>
+void Aplicacion<T>::addPre_rqs(char *key)
+{
+    std::string temp;
+    cout << "Ingrese codigo prerequisito: ";
+    cin>>temp;
+    temp.insert(0,"\n,");
+    Requisitos->write_in_file(key,temp);
+}
+
+template <class t>
+vector<string>* Aplicacion<t>::find_prerqs(char *key)
+{
+    std::vector<string> *prerequisitos_V = new vector<string> ;
+    prerequisitos_V->push_back(Requisitos->GetRecord(key)); // Encuentra el prerequisito y le da al vector
+    return prerequisitos_V;
 }
 
 template <class T>
@@ -53,13 +77,13 @@ void Aplicacion<T>::startPrinting()
     do
     {
     std::cout << "INGRESE LA OPCION: "<<std::endl;
-    std::cout << "1) FIRST\n2) PREV\n3)NEXT\n4)LAST \n";
+    std::cout << "1) FIRST\n2) PREV\n3)NEXT\n4)LAST\n5)FIND\n6)VER ORDENADO\n";
     cin>>opcion;
     switch(opcion)
     {
         case 1:
             Cursos->printFirst();
-            Current = Current = Cursos->m_record.head;
+            Current = Cursos->m_record.head;
             break;
         case 2:
             Cursos->printPrev(Current);
@@ -71,9 +95,100 @@ void Aplicacion<T>::startPrinting()
             Cursos->printLast();
             Current = Cursos->m_record.last;
             break;
+        case 5:
+            find_Subject();
+            break;
+        case 6:
+            print_for_code();
+            break;
     }
     cout << "salir? (0/1)" << endl;
     cin>>salir;
     }while(salir!= 1);
+}
+
+template <class T>
+void Aplicacion<T>::find_Subject()
+{
+    char elem[9];
+    cout << "Ingrese el codigo del curso: "<< endl;
+    cin >> elem;
+    Cursos->GetRecord(elem);
+    find_prerqs(elem);  // imprime requisitos
+}
+
+template <class T>
+char* Aplicacion<T>::year_key(char *key)
+{
+    char  nuevo;
+    for(int i = 0; i< 3 ; i++ )
+    {
+        nuevo = key[i];
+    }
+    return &nuevo;
+}
+
+template <class T>
+void Aplicacion<T>::print_for_code()
+{
+    Nodo<Record> * temp = Cursos->m_record.head;
+    //years
+    std::vector<char*> first_year;
+    std::vector<char*> second_year;
+    std::vector<char*> third_year;
+    std::vector<char*> forth_year;
+    std::vector<char*> fifth_year;
+    char *year;
+    while(temp!=nullptr)
+    {
+        year = year_key((temp->dato).key);
+        switch(*year)
+        {
+        case '1':
+            first_year.push_back((temp->dato).key);
+            break;
+        case '2':
+            second_year.push_back((temp->dato).key);
+            break;
+        case '3':
+            third_year.push_back((temp->dato).key);
+            break;
+        case '4':
+            forth_year.push_back((temp->dato).key);
+            break;
+        case '5':
+            fifth_year.push_back((temp->dato).key);
+            break;
+        }
+        temp = temp->sig;
+    }
+    cout << "IMPRIMIENDO POR AÑOS: " << endl;
+    cout << "--------------------First Year ------------------- " <<endl;
+    for(std::vector<char*>::iterator tem = first_year.begin(); tem != first_year.end();tem++)
+    {
+        Cursos->GetRecord(*tem);
+    }
+    cout << "--------------------Second year ------------------" <<endl;
+    for(std::vector<char*>::iterator tem = second_year.begin(); tem != second_year.end();tem++)
+    {
+        Cursos->GetRecord(*tem);
+    }
+    cout << "--------------------third year ------------------" <<endl;
+    for(std::vector<char*>::iterator tem = third_year.begin(); tem != third_year.end();tem++)
+    {
+        Cursos->GetRecord(*tem);
+    }
+    cout << "--------------------fourth year ------------------" <<endl;
+    for(std::vector<char*>::iterator tem = forth_year.begin(); tem != forth_year.end();tem++)
+    {
+        Cursos->GetRecord(*tem);
+    }
+    cout << "--------------------fifth year ------------------" <<endl;
+    for(std::vector<char*>::iterator tem = fifth_year.begin(); tem != fifth_year.end();tem++)
+    {
+        Cursos->GetRecord(*tem);
+    }
+
+
 }
 #endif // APP_H
--- ListSE/Table.h
+++ ListSE/Table.h
@@ -3,6 +3,7 @@
 #include "List.h"
 #include <fstream>
 #include <cstring>
+#include <string>
 #include <iostream>
 
 using namespace std;
@@ -34,19 +35,29 @@ class Table{
     //public:
         Table(char *file);
         void index();
-        T GetRecord(char *key);
+        string GetRecord(char *key);
+
         void printKey();
-        void StartPrinting();
         void printNext(Nodo<T>* &temp);
         void printPrev(Nodo<T>* &temp);
         void printFirst();
         void printLast();
+        void write_in_file(char *key, std::string &temp);
 };
 
 template<class T>
 Table<T> :: Table(char *file){
     this->file = file;
     Current = nullptr;
+}
+
+template <class T>
+void Table<T>::write_in_file(char* key, string &temp)
+{
+    std::ofstream out;
+    out.open(file, std::ios::app);
+    out<<temp;
+    out.close();
 }
 
 template<class T>
@@ -63,22 +74,18 @@ void Table<T> :: index(){
 
         in.getline(key, 11, ','); /** capturamos los 11 primeros caracteres o hasta encontrar una coma **/
         in.getline(buffer, 1000); /** capturamos los siguientes 1000 caracteres **/
-
-        std::cout << key << buffer << std::endl;
-
-
         Record R;
         strcpy(R.key , key); /** copiamos el contenido de key en el key de Record **/
         R.start = offset; /** guardamos el inicio del registro **/
         m_record.pushBack(R); /** ingresamos el registro a lista **/
-        offset += strlen(key) + strlen(buffer) + 2; /** actualizamos la variable para el inicio de la siguiente linea **/
+        offset += strlen(key) + strlen(buffer) + 3; /** actualizamos la variable para el inicio de la siguiente linea **/
     }
 
     in.close();
 }
 
 template<class T>
-T Table<T> :: GetRecord(char * key)
+string Table<T> :: GetRecord(char * key)
 {
        char data[1000];
        Record *r = m_record.findElem(key, new Record); /** funcion especial que devuelve el puntero de el Record buscado **/
@@ -87,9 +94,13 @@ T Table<T> :: GetRecord(char * key)
        if(r){
            std::ifstream file(this->file,ios::in | ios::binary); /** abrimos el archivo para manipularlo **/
            file.seekg(r->start, std::ios_base::beg); /** Nos posicionamos en el archivo segun la posicion guardada del registro **/
+           pos = file.tellg();
+           std::cout<<" posicion "<< r->start << " " << std::endl;
            file.getline(data, 1000); /** capturamos los la linea **/
            pos = file.tellg();
-           std::cout<<":::"<< data <<"posicion"<< pos << std::endl;
+           std::cout<<"::: "<< data <<" posicion"<< pos << " " << std::endl;
+           string str = string(data);
+           return str;
        }
        else
        {
@@ -100,40 +111,6 @@ T Table<T> :: GetRecord(char * key)
 template<class T>
 void Table<T> :: printKey(){
     m_record.printStruct();
-}
-
-template <class T>
-void Table<T>::StartPrinting()
-{
-    Current = m_record.head;
-    GetRecord((Current->dato).key);
-    int opcion;
-    bool salir;
-    do
-    {
-    std::cout << "INGRESE LA OPCION: "<<std::endl;
-    std::cout << "1) FIRST\n2) PREV\n3)NEXT\n4)LAST \n";
-    cin>>opcion;
-    switch(opcion)
-    {
-        case 1:
-            printFirst();
-            Current = m_record.head;
-            break;
-        case 2:
-            printPrev(Current);
-            break;
-        case 3:
-            printNext(Current);
-            break;
-        case 4:
-            printLast();
-            Current = m_record.last;
-            break;
-    }
-    cout << "salir? (0/1)" << endl;
-    cin>>salir;
-    }while(salir!= 1);
 }
 
 template <class T>
